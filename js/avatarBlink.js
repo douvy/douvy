c1f6bb1ef87814eddde7161bc893e46acbe19c1f
douvy <rjd5146@gmail.com>
2023-06-02 00:07:36 -0400

    added bloodshot eyes to pfp after 30 seconds and removed old images

--- js/avatarBlink.js
+++ js/avatarBlink.js
@@ -1,14 +1,31 @@
 var normalImage = "assets/img/pfp.jpg";
 var blinkingImage = "assets/img/pfp-blink.jpg";
+var bloodshotImage = "assets/img/pfp-bloodshot.jpg";
 var imgElement = document.getElementById("pfp");
-var durations = [150, 200, 250]; // blink durations to choose from
+var durations = [150, 200, 250];
+var isBloodshot = false;
+var startTime = Date.now();
 
 setInterval(function() {
-    if (imgElement.src.endsWith(normalImage)) {
-        var duration = durations[Math.floor(Math.random() * durations.length)]; // randomly select a blink duration
-        imgElement.src = blinkingImage;
-        setTimeout(function() {
-            imgElement.src = normalImage;
-        }, duration);
-    } 
-}, 4000); // blink interval
+    var currentTime = Date.now();
+    var elapsedSeconds = (currentTime - startTime) / 1000;
+
+    if (elapsedSeconds >= 30 && !isBloodshot) {
+        imgElement.src = bloodshotImage;
+        isBloodshot = true;
+    } else {
+        var duration = durations[Math.floor(Math.random() * durations.length)];
+        
+        if (isBloodshot) {
+            imgElement.src = blinkingImage;
+            setTimeout(function() {
+                imgElement.src = bloodshotImage;
+            }, duration);
+        } else {
+            imgElement.src = blinkingImage;
+            setTimeout(function() {
+                imgElement.src = normalImage;
+            }, duration);
+        }
+    }
+}, 4000);
