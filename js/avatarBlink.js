faea62d10e8c9968ba03ea4ef67855ba89946834
douvy <rjd5146@gmail.com>
2023-06-01 23:08:54 -0400

    fixed blinking animations to loop endlessly

--- js/avatarBlink.js
+++ js/avatarBlink.js
@@ -1,34 +1,14 @@
-    var normalImage = "assets/img/pfp.jpg";
-    var blinkingImage = "assets/img/pfp-blink.jpg";
-    var imgElement = document.getElementById("pfp");
-    var blinkCount = 0;
+var normalImage = "assets/img/pfp.jpg";
+var blinkingImage = "assets/img/pfp-blink.jpg";
+var imgElement = document.getElementById("pfp");
+var durations = [150, 200, 300]; // blink durations to choose from
 
-    setInterval(function() {
-        if (imgElement.src.endsWith(normalImage)) {
-            if (blinkCount === 0) {
-                imgElement.src = blinkingImage;
-                setTimeout(function() {
-                    imgElement.src = normalImage;
-                }, 200); // 200 milliseconds
-            } else if (blinkCount === 1) {
-                imgElement.src = blinkingImage;
-                setTimeout(function() {
-                    imgElement.src = normalImage;
-                }, 150); // 150 milliseconds
-            } else if (blinkCount === 2) {
-                imgElement.src = blinkingImage;
-                setTimeout(function() {
-                    imgElement.src = normalImage;
-                }, 200); // 200 milliseconds
-            } else if (blinkCount === 3) {
-                imgElement.src = blinkingImage;
-                setTimeout(function() {
-                    imgElement.src = normalImage;
-                }, 300); // 300 milliseconds
-                blinkCount = -1; // reset blinkCount to -1 to start over
-            }
-            blinkCount++;
-        } else {
+setInterval(function() {
+    if (imgElement.src.endsWith(normalImage)) {
+        var duration = durations[Math.floor(Math.random() * durations.length)]; // randomly select a blink duration
+        imgElement.src = blinkingImage;
+        setTimeout(function() {
             imgElement.src = normalImage;
-        }
-    }, 4000); // 5000 milliseconds = 5 seconds
+        }, duration);
+    } 
+}, 4000); // blink interval
