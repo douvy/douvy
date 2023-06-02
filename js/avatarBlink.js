8a167c8aaa14cd16a1c2544ed9137d2738e39558
douvy <rjd5146@gmail.com>
2023-06-02 11:09:12 -0400

    made it so pfp has no blunt for 5 seconds then starts smoking it

--- js/avatarBlink.js
+++ js/avatarBlink.js
@@ -1,10 +1,22 @@
 var normalImage = "assets/img/pfp.jpg";
 var blinkingImage = "assets/img/pfp-blink.jpg";
 var bloodshotImage = "assets/img/pfp-bloodshot.jpg";
+var initialImage = "assets/img/pfp-bluntless.jpg";  // initial image
+
 var imgElement = document.getElementById("pfp");
+
 var durations = [150, 200, 250];
 var isBloodshot = false;
 var startTime = Date.now();
+
+// Set initial image
+imgElement.src = initialImage;
+
+// Change to normal image after 5 seconds
+setTimeout(function() {
+    imgElement.src = normalImage;
+    startTime = Date.now();  // reset the start time
+}, 5000);
 
 setInterval(function() {
     var currentTime = Date.now();
