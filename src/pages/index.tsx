2e34aec400b911633d43223990a535e115a29b24
douvy <itsdouvy@gmail.com>
2026-02-16 12:32:40 -0500

    content: update Propix img

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -617,8 +617,8 @@ export default function Home(): React.ReactElement {
                           id="propix"
                           images={[
                             {
-                              src: "/img/propix.jpg",
-                              id: "propix",
+                              src: "/img/propix-1.jpg",
+                              id: "propix-1",
                               alt: "Propix Agent",
                             },
                           ]}
